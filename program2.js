5b2cbe152e9bf89a4e2a2de36445421c977c05f6
Sujaljadiya <sujalchhatarpur@gmail.com>
2024-04-26 12:30:01 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,27 @@
 function longestSubstring(s) {
 
 
-    // Implementation of longestSubstring function
+    const charIndexMap = {};
+  let left = 0;
+  let maxLen = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    const currentChar = s[right];
+
+    // If the character is already seen, update the left pointer to skip past the previous occurrence
+    if (charIndexMap.hasOwnProperty(currentChar) && charIndexMap[currentChar] >= left) {
+      left = Math.max(left, charIndexMap[currentChar] + 1);
+    }
+
+    // Update the last seen index for the current character
+    charIndexMap[currentChar] = right;
+
+    // Update the max length if the current substring is longer
+    maxLen = Math.max(maxLen, right - left + 1);
+  }
+
+  return maxLen;
+}
 }
 
 module.exports = { longestSubstring };
