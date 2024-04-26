340ff14481fed309a162a170eed1ddb3ec0cd8c2
Sujaljadiya <sujalchhatarpur@gmail.com>
2024-04-26 12:47:20 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,7 @@ function smallestMissingPositiveInteger(nums) {
 
   // Mark the presence of non-negative integers using their indices.
   for (let i = 0; i < n; i++) {
-    let correctIndex = arr[i];
+    let correctIndex = nums[i];
     // Ensure correctIndex is within valid range (0 to n-1)
     correctIndex = Math.max(Math.min(correctIndex, n - 1), 0);
 
