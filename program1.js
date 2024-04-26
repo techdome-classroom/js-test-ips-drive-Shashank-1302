ab7adb30022867a74bd88fd751f8c1115b8b45a3
Sujaljadiya <sujalchhatarpur@gmail.com>
2024-04-26 12:24:34 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -4,25 +4,20 @@ function smallestMissingPositiveInteger(nums) {
   const n = nums.length;
   for (let i = 0; i < n; i++) {
     while (0 <= nums[i] < n && nums[i] != i) {
-      // Swap the number with the value at its correct index
       const temp = nums[i];
       nums[i] = nums[temp];
       i = temp;
     }
   }
 
-  // Find the first number that is not at its correct position (i.e., the smallest missing positive integer)
   for (let i = 0; i < n; i++) {
     if (nums[i] != i + 1) {
       return i + 1;
     }
   }
-
-  // If all numbers are present, return n + 1 (smallest missing positive integer)
   return n + 1;
 }
   
-  }
   
   module.exports = smallestMissingPositiveInteger;
 
