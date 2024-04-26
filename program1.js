ac51ef630a88ffa0a15fe902af78795cb643e8b8
Sujaljadiya <sujalchhatarpur@gmail.com>
2024-04-26 12:40:36 +0530

    Auto commit on save

---
+++ program1.js
@@ -0,0 +1,23 @@
+function smallestMissingPositiveInteger(nums) {
+
+    const n = nums.length;
+    for (let i = 0; i < n; i++) {
+      while (0 <= nums[i] < n && nums[i] != i && nums[i] !== 0) {
+        const temp = nums[i];
+        nums[i] = nums[temp];
+        i = temp;
+      }
+    }
+  
+    for (let i = 0; i < n; i++) {
+      if (nums[i] != i + 1) {
+        return i + 1;
+      }
+    }
+  
+    return n + 1;
+  }
+
+  
+  module.exports = smallestMissingPositiveInteger;
+
