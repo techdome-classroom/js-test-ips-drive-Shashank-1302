2e94cf37310378575560c3e68429dd72b661f594
Sujaljadiya <sujalchhatarpur@gmail.com>
2024-04-26 12:48:49 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,5 +1,22 @@
 function smallestMissingPositiveInteger(nums) {
-  
+    // Handle the base case where the list is empty
+    if (!nums.length) return 1;
+
+    const positiveNumbers = new Set();
+   for (const num of nums) {
+     if (num > 0) {
+       positiveNumbers.add(num);
+     }
+   }
+ 
+   for (let i = 1; i <= positiveNumbers.size + 1; i++) {
+     if (!positiveNumbers.has(i)) {
+       return i;
+     }
+   }
+ 
+   return positiveNumbers.size + 1;
+ }
 
 
   
